d241caca891793ef036fcd487920f57b166edea5
Ivan Degtyarev <st051982@student.spbu.ru>
2020-09-23 02:03:07 +0300

    code style fix hw1.2

--- homework_1/hw1.2/main.c
+++ homework_1/hw1.2/main.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
 #include <malloc.h>
+#include <stdio.h>
 
-void swap(int *a, int *b) {
+void swap(int* a, int* b)
+{
     int tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-void reverseArray(int* numbers, int first, int last) {
+void reverseArray(int* numbers, int first, int last)
+{
     for (int i = 0; i <= (last - first) / 2; ++i) {
         swap(&numbers[first + i], &numbers[last - i]);
     }
 }
 
-int main() {
+int main()
+{
     int m = 0;
     printf("Enter the length of the first part of the array :\n");
     scanf("%d", &m);
@@ -22,7 +25,7 @@ int main() {
     printf("Enter the length of the second part of the array :\n");
     scanf("%d", &n);
 
-    int *numbers = (int*)malloc((n + m) * sizeof(int));
+    int* numbers = (int*)malloc((n + m) * sizeof(int));
     printf("Enter the array : ");
     for (int i = 0; i < n + m; ++i) {
         scanf("%d", &numbers[i]);
