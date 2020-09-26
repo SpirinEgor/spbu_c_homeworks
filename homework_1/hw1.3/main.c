ac481c15aaf8e161594c531dda388cb8aa8364ed
Ivan Degtyarev <st051982@student.spbu.ru>
2020-09-26 21:50:04 +0300

    replace inputString() to scanf()

--- homework_1/hw1.3/main.c
+++ homework_1/hw1.3/main.c
@@ -1,42 +1,29 @@
 #include <malloc.h>
 #include <stdbool.h>
+#include <string.h>
 #include <stdio.h>
 
-void inputString(char* myString, int length)
-{
-    char tmp = 0;
-    tmp = getchar();
-    for (int i = 0; i < length; ++i) {
-        tmp = getchar();
-        myString[i] = tmp;
-    }
-}
+const int MAX_SIZE = 100;
 
 int main()
 {
-    int n = 0;
-    printf("Enter the length of the string 's1' :\n");
-    scanf("%d", &n);
-
-    int m = 0;
-    printf("Enter the length of the string 's2' :\n");
-    scanf("%d", &m);
-
-    char* s = (char*)malloc(n * sizeof(char));
+    char* s = (char*)malloc(MAX_SIZE * sizeof(char));
     printf("Enter the string 's1' :\n");
-    inputString(s, n);
+    scanf("%s", s);
+    int sSize = strlen(s);
 
-    char* s1 = (char*)malloc(m * sizeof(char));
+    char* s1 = (char*)malloc(MAX_SIZE * sizeof(char));
     printf("Enter the string 's2' :\n");
-    inputString(s1, m);
+    scanf("%s", s1);
+    int s1Size = strlen(s1);
 
     int substringCounter = 0;
     bool areEqual = true;
     int j = 0;
-    for (int i = 0; i < (n - m + 1); ++i) {
+    for (int i = 0; i < (sSize - s1Size + 1); ++i) {
         areEqual = true;
         j = 0;
-        while (j < m && areEqual) {
+        while (j < s1Size && areEqual) {
             if (s[i + j] != s1[j])
                 areEqual = false;
             ++j;
