a9c540b1f8e3946c512edd9151bf7832fd84e355
Ivan Degtyarev <st051982@student.spbu.ru>
2020-09-27 17:51:08 +0300

    hw2.3 added

---
+++ homework_2/hw2.3/main.c
@@ -0,0 +1,33 @@
+#include "../../library/commonUtils/numericOperations.h"
+#include "malloc.h"
+#include "stdio.h"
+
+void nullsToEnd(int* numbers, int n) {
+    for (int i = n - 1; i > 0; --i) { //bubblesort-like
+        for (int j = 0; j < i; ++j) {
+            if (numbers[j] == 0 && numbers[j +1] != 0)
+                swap(&numbers[j], &numbers[j + 1]);
+        }
+    }
+}
+
+int main()
+{
+    int n = 0;
+    printf("Enter size of the array:\n");
+    scanf("%d", &n);
+
+    int* numbers = (int*)calloc(n, sizeof(int));
+    printf("Enter the array:\n");
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &numbers[i]);
+    }
+
+    nullsToEnd(numbers, n);
+
+    printf("Entered array with moved nulls:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", numbers[i]);
+    }
+
+}
--- library/commonUtils/numericOperations.h
+++ library/commonUtils/numericOperations.h
@@ -2,6 +2,9 @@
 #define SPBU_2020_C_NUMERICOPERATIONS_H
 
 int min(int a, int b);
+
 int max(int a, int b);
+
+void swap(int* a, int* b);
 
 #endif //SPBU_2020_C_NUMERICOPERATIONS_H
