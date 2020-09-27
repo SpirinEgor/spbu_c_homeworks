89e2c5c67d4a889e143a68353fe6afe9e15a1831
Ivan Degtyarev <st051982@student.spbu.ru>
2020-09-27 14:46:25 +0300

    hw2.4 added

---
+++ homework_2/hw2.4/main.c
@@ -0,0 +1,36 @@
+#include "malloc.h"
+#include <stdio.h>
+#include "../../library/commonUtils/mysorts.h"
+
+void showNumerals(int* numerals, int n)
+{
+    for (int i = 0; i < n; ++i)
+        printf("%d", numerals[i]);
+}
+
+void inputNumerals(int* numerals, int n)
+{
+    char tmp = 0;
+    for (int i = 0; i < n; ++i) {
+        tmp = getchar();
+        numerals[i] = tmp - '0';
+    }
+}
+
+int main()
+{
+    int n = 0;
+    printf("Enter the length of number :\n");
+    scanf("%d", &n);
+
+    char tmp = 0;
+    printf("Enter the number :\n");
+    int* numerals = calloc(n, sizeof(int));
+
+    tmp = getchar();
+    inputNumerals(numerals, n);
+    countingSort(numerals, n, 0, 9);
+    showNumerals(numerals, n);
+
+    free(numerals);
+}
