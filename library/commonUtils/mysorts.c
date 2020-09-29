690e50f605475ed944a1805b8de1fb597fd1b76f
Ivan Degtyarev <st051982@student.spbu.ru>
2020-09-29 17:15:13 +0300

    fix codestyle

--- library/commonUtils/mysorts.c
+++ library/commonUtils/mysorts.c
@@ -4,10 +4,7 @@
 void countingSort(int* numbers, int n, int first, int last)
 {
     int length = last - first + 1;
-    int* counters = malloc(length * sizeof(int));
-    for (int i = 0; i < length; ++i) {
-        counters[i] = 0;
-    }
+    int* counters = calloc(length, sizeof(int));
 
     for (int i = 0; i < n; ++i) {
         ++counters[numbers[i] - first];
@@ -19,7 +16,7 @@ void countingSort(int* numbers, int n, int first, int last)
             ++j;
         }
         numbers[i] = first + j;
-        counters[j]--;
+        --counters[j];
     }
     free(counters);
 }
